723bbefef96b5fabfca5080e767fca07c37d55d5
Roguebugger <roguebugger@gmail.com>
2019-05-19 19:55:28 +0530

    new changes

--- workspace/schedule the week/schedule.py
+++ workspace/schedule the week/schedule.py
@@ -1,7 +1,10 @@
-import pandas as pd 
+import pandas as pd
+import numpy as np 
+
+import tensorflow as tf
+
 
 data=pd.read_csv('data.csv')
-
 
 
 class job:
@@ -9,6 +12,15 @@ class job:
         self.job_name=name
         self.job_due_date=due_date
         self.job_requirement=requirement
+
+class week:
+    def __init__(self)
+    self.free_time=None
+    self.days=[None]
+    
+
+
+
 
 
 datas=[data['job_name'],data['due_date'],data['profit']]
