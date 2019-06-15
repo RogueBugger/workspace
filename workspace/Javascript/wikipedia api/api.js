2d67e1057049f276c8a3acc08c2dd1b7ac8abd4d
Roguebugger <roguebugger@gmail.com>
2019-06-15 23:22:53 +0530

    new javascript file

--- workspace/Javascript/wikipedia api/api.js
+++ workspace/Javascript/wikipedia api/api.js
@@ -1,36 +1,35 @@
-let searchUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=';
-let contentUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=';
+    let searchUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=';
+    let contentUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=';
 
-let userInput;
+    let userInput;
 
-let counter = 0;
-var text;
-var data;
-function setup() {
-  noCanvas();
-  userInput = select('#userinput');
-  userInput.changed(goWiki);
-  goWiki();
+    let counter = 0;
+    function setup() {
+      noCanvas();
+      userInput = select('#userinput');
+      userInput.changed(goWiki);
+      goWiki();
 
-  function goWiki(){
-      let term = userInput.value();
-      let url = searchUrl + term;
-      loadJSON(url, gotData, 'jsonp');
-  }
-  function gotData(data){
-      data = data;
-      console.log(data);
-      console.log(data[2]);
-      text = data[2][0];
-      assign(text);
-      
+      function goWiki(){
+          let term = userInput.value();
+          let url = searchUrl + term;
+          loadJSON(url, gotData, 'jsonp');
+          
+      }
+      function gotData(data){
+          for (let i = 0 ; i < data[2].length ; i++){
+          createP(toPreferred(data[2][i])); 
+          }
+          
+      }
+      function toPreferred(text){
+        for(let i = 0; i<text.length ; i++){
+            if(text[i] === '[' || text[i] === "]" || text[i] === '(' || text[i] === ')' || !isNaN(text[i])){
+                text = text.replace(text[i], " ");
+            }
+
+            
+        }        
+        return text;
+      }
     }
-  function assign(text){
-      text = text;
-  }
-}
-var t = text;
-console.log(t+'helaskdfl;ahsldfh');
-
-
-document.write(t);
--- workspace/Javascript/wikipedia api/sketch.js
+++ workspace/Javascript/wikipedia api/sketch.js
@@ -1,10 +1,3 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-
-// Wikipedia
-// Edited Video: https://youtu.be/RPz75gcHj18
-
 let searchUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=';
 let contentUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=';
 
